ac330b753aed72aeb11336fb9f585e9d10b7fa56
Artem Petrenko <artem.petrenko@cactussoft.biz>
2018-06-20 13:59:58 +0300

    no message

--- node.lua
+++ node.lua
@@ -640,7 +640,7 @@ local function JobQueue()
     local jobs = {}
 
     local function add(fn, starts, ends, coord)
-		if #jobs > 0 and #jobs <= index and jobs[index].index == index then
+		if #jobs > 0 and #jobs >= index and jobs[index].index == index then
 			jobs[index].starts = starts
 			jobs[index].ends = ends
 		else 
