b05b67f6b7f85ce4f80a9d18b0b0f60306550ae9
Artem Petrenko <artem.petrenko@cactussoft.biz>
2018-06-21 14:12:17 +0300

    no message

--- node.lua
+++ node.lua
@@ -721,7 +721,7 @@ local function Scheduler(playlist_source, job_queue)
 		print("SCHEDULE START")
 		
 		if not playlist then
-			playlist = playlist_source
+			playlist = playlist_source()
 		end
 
         -- get total playlist duration
