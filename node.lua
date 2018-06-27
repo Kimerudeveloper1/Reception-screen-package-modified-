cbd6d0d2b135a02355ddc971d6ea4ddc2ad3f1cd
Artem Petrenko <artem.petrenko@cactussoft.biz>
2018-06-27 15:05:39 +0300

    no message

--- node.lua
+++ node.lua
@@ -1016,7 +1016,7 @@ local function rotate(degree)
     elseif degree == 90 then
         WIDTH, HEIGHT = HEIGHT, WIDTH
         return function()
-			gl.translate(HEIGHT, 0)
+			
             gl.rotate(90, 0, 0, 1)
         end
     elseif degree == 180 then
